b1610e1b4f6a32da1be6326394d1871a8750c788
devinmatter <francoinformatica2017@gmail.com>
2023-02-04 07:38:30 -0300

    Add Systray and other changes.

--- config.h
+++ config.h
@@ -1,23 +1,32 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10:weight=Medium",
-					"JoyPixels:size=10:antialias=true:autohint=true" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft  = 0;   /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        	 = 1;   /* 0 means no systray */
+
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10:weight=Bold",
+                                        "JoyPixels:size=10:antialias=true:autohint=true",
+                                        // "Sarasa Gothic:pixelsize=100:antialias=true:autohint=true"
+};
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10:weight=Medium";
+
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+        /*               fg         bg         border   */
+        [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+        [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
 /* tagging */
@@ -29,8 +38,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",     	NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  	NULL,       NULL,       1 << 8,       0,           -1 },
+    	{ "feh",      	NULL,       NULL,       0,            1,           -1 },
+    	{ "mpv",      	NULL,       NULL,       0,            1,           -1 },
+    	{ "Galculator",	NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
@@ -44,7 +56,6 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "[M]",      monocle },
 	{ "|||",      col },
-	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ NULL,      NULL },    /* no layout function means floating behavior */
 };
 
@@ -81,14 +92,14 @@ static Key keys[] = {
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_z,      zoom,           {0} },
-	//{ MODKEY,                       XK_Tab,    view,           {0} },
+	/* { MODKEY,                       XK_Tab,    view,           {0} }, */
     	{ MODKEY,                       XK_Tab,    cyclelayout, {.i = +1 } },
     	{ MODKEY|ShiftMask,             XK_Tab,    cyclelayout,  {.i = -1 } },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[3]} },
+	// { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
     	{ MODKEY|ShiftMask,		XK_f,	   togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -97,6 +108,11 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+    	{ MODKEY|ShiftMask,		XK_w,  	   spawn, SHCMD("nitrogen --set-zoom-fill --random") },
+    	{ MODKEY|ShiftMask,		XK_c,  	   spawn, SHCMD("$HOME/.scripts/chxkbmap") },
+    	{ MODKEY|ShiftMask,		XK_p,  	   spawn, SHCMD("$HOME/.scripts/toggle-picom") },
+    	{ MODKEY|ShiftMask,		XK_r,  	   spawn, SHCMD("$HOME/.scripts/toggle-redshift") },
+    	{ MODKEY|ShiftMask,		XK_e,  	   spawn, SHCMD("st -e ranger") },
     	{ 0,                            XK_Print,  spawn, SHCMD("scrot %Y-%m-%d-%T-screenshot.png -e 'xclip -selection clipboard -t image/png -i $f; mv $f ~/Pictures/screenshots/' &>/dev/null") },
     	{ MODKEY,                            XK_s,  spawn, SHCMD("scrot --select %Y-%m-%d-%T-cut.png -e 'xclip -selection clipboard -t image/png -i $f; mv $f ~/Pictures/cuts/' &>/dev/null") },
     	{ 0, XF86XK_AudioLowerVolume,   spawn,     SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -5%") },
@@ -123,8 +139,10 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	// { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	// { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+       	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+       	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
