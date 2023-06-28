4c097eed127da722443a1bccfcd66997c1dcb458
fixztter <francoinformatica2017@gmail.com>
2023-06-28 11:33:22 +0000

    Modify motion.

--- dwm.c
+++ dwm.c
@@ -1389,7 +1389,8 @@ movemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			// if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			if ((ev.xmotion.time - lasttime) <= (1000 / 1000))
 				continue;
 			lasttime = ev.xmotion.time;
 
